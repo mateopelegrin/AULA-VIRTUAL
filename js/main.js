ec2c068bafdfc30c02a2c592c6049c720c40a43f
Mateo Pelegrin <matepelegrin@gmail.com>
2023-03-26 17:29:37 -0300

    Pre-entrega3

--- js/main.js
+++ js/main.js
@@ -1,71 +1,99 @@
-class Producto {
-    constructor(nombre, cantidad, precio) {
-        this.nombre = nombre;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-}
-
-const catalogo = [
-    { nombre: 'Manzanas', precio: 2.5 },
-    { nombre: 'Peras', precio: 3 },
-    { nombre: 'Naranjas', precio: 4 },
-    { nombre: 'Bananas', precio: 1.5 },
+// Definir los productos disponibles
+const productos = [
+    { nombre: "Manzanas", precio: 2.50, cantidad: 10 },
+    { nombre: "Naranjas", precio: 2.00, cantidad: 15 },
+    { nombre: "Bananas", precio: 1.50, cantidad: 12 },
+    { nombre: "Papas", precio: 1.00, cantidad: 20 },
+    { nombre: "Zanahorias", precio: 1.50, cantidad: 18 }
 ];
 
-class Verduleria {
-    constructor() {
-        this.productos = [];
-    }
+// Agregar los productos a la página
+const listaProductos = document.getElementById("productos");
+productos.forEach(producto => {
+    const item = document.createElement("li");
+    item.innerHTML = `${producto.nombre} - $${producto.precio.toFixed(2)} - Disponibles: ${producto.cantidad}`;
+    const boton = document.createElement("button");
+    boton.innerHTML = "Agregar al carrito";
+    boton.addEventListener("click", () => agregarAlCarrito(producto));
+    item.appendChild(boton);
+    listaProductos.appendChild(item);
+});
 
-    agregarProducto(producto) {
-        const index = this.productos.findIndex(p => p.nombre === producto.nombre);
-        if (index !== -1) {
-            this.productos[index].cantidad += producto.cantidad;
-        } else {
-            this.productos.push(producto);
-        }
-    }
+// Definir las variables para el carrito de compras y el total
+let carrito = [];
+let total = 0;
 
-    calcularPrecioTotal() {
-        let total = 0;
-        for (let producto of this.productos) {
-            total += producto.cantidad * producto.precio;
-        }
-        return total;
-    }
-
-    agregarProductosDelCatalogo() {
-        let seguirAgregando = true;
-        while (seguirAgregando) {
-            let catalogoMensaje = 'Este es nuestro catálogo de productos:\n\n';
-            for (let i = 0; i < catalogo.length; i++) {
-                catalogoMensaje += `${i + 1}. ${catalogo[i].nombre} - $${catalogo[i].precio}\n`;
-            }
-            alert(catalogoMensaje);
-            const opcion = parseInt(prompt('Ingrese el número del producto que desea agregar, o 0 para terminar.'));
-            if (opcion === 0) {
-                seguirAgregando = false;
-            } else if (opcion >= 1 && opcion <= catalogo.length) {
-                const productoElegido = catalogo[opcion - 1];
-                const cantidad = parseInt(prompt(`Cuántas ${productoElegido.nombre} desea agregar?`));
-                const producto = new Producto(productoElegido.nombre, cantidad, productoElegido.precio);
-                this.agregarProducto(producto);
-                alert(`Se agregaron ${cantidad} ${productoElegido.nombre} a la verdulería.`);
-            } else {
-                alert('Opción inválida, por favor intente nuevamente.');
-            }
-        }
+// Función para agregar un producto al carrito de compras
+function agregarAlCarrito(producto) {
+    if (producto.cantidad > 0) {
+        carrito.push(producto);
+        producto.cantidad--;
+        actualizarCarrito();
+        actualizarProductos();
     }
 }
 
-const verduleria = new Verduleria();
-verduleria.agregarProductosDelCatalogo();
-
-let carritoMensaje = 'Los productos en su carrito son:\n\n';
-for (let producto of verduleria.productos) {
-    carritoMensaje += `${producto.cantidad} ${producto.nombre} - $${producto.cantidad * producto.precio}\n`;
+// Función para actualizar el carrito de compras en la página
+function actualizarCarrito() {
+    const listaCarrito = document.getElementById("carrito");
+    listaCarrito.innerHTML = "";
+    carrito.forEach(producto => {
+        const item = document.createElement("li");
+        item.innerHTML = `${producto.nombre} - $${producto.precio.toFixed(2)}`;
+        listaCarrito.appendChild(item);
+    });
+    actualizarTotal();
 }
-const precioTotalMensaje = `El precio total es: $${verduleria.calcularPrecioTotal()}`;
 
-alert(carritoMensaje + precioTotalMensaje);
+// Función para actualizar la lista de productos disponibles en la página
+function actualizarProductos() {
+    const listaProductos = document.getElementById("productos");
+    listaProductos.innerHTML = "";
+    productos.forEach(producto => {
+        const item = document.createElement("li");
+        item.innerHTML = `${producto.nombre} - $${producto.precio.toFixed(2)} - Disponibles: ${producto.cantidad}`;
+        const boton = document.createElement("button");
+        boton.innerHTML = "Agregar al carrito";
+        boton.addEventListener("click", () => agregarAlCarrito(producto));
+        item.appendChild(boton);
+        listaProductos.appendChild(item);
+    });
+}
+
+
+// Función para actualizar el total a pagar en la página
+function actualizarTotal() {
+    total = carrito.reduce((suma, producto) => suma + producto.precio, 0);
+    const totalElemento = document.getElementById("total");
+    totalElemento.innerHTML = `Total a pagar: $${total.toFixed(2)}`;
+}
+
+// Función para procesar el pago
+function procesarPago() {
+    // Verificar que haya productos en el carrito
+    if (carrito.length === 0) {
+        alert("El carrito de compras está vacío");
+        return;
+    }
+
+    // Confirmar la compra
+    const confirmar = confirm(`Total a pagar: $${total.toFixed(2)}\n¿Desea confirmar la compra?`);
+    if (!confirmar) {
+        return;
+    }
+
+    // Restablecer el carrito y actualizar la lista de productos disponibles
+    carrito.forEach(producto => {
+        const productoOriginal = productos.find(p => p.nombre === producto.nombre);
+        productoOriginal.cantidad++;
+    });
+    carrito = [];
+    actualizarCarrito();
+    actualizarProductos();
+    actualizarTotal();
+    alert("Gracias por su compra");
+}
+
+// Agregar el evento de click al botón de pago
+const botonPagar = document.getElementById("pagar");
+botonPagar.addEventListener("click", procesarPago);
